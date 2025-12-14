f33f6919a45c0998cb81eb26d720cec9ef6a88ea
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-12-14 16:05:49 +0100

    Enable showUpdatedTime by default in RepositoryMenu
    Changed the default value of the showUpdatedTime property in the RepositoryMenu data class from false to true, so updated time will now be shown by default.

--- datastore/src/main/kotlin/com/dergoogler/mmrl/datastore/model/RepositoryMenu.kt
+++ datastore/src/main/kotlin/com/dergoogler/mmrl/datastore/model/RepositoryMenu.kt
@@ -14,7 +14,7 @@ data class RepositoryMenu
         @ProtoNumber(4) val pinUpdatable: Boolean = true,
         @ProtoNumber(5) val showIcon: Boolean = true,
         @ProtoNumber(6) val showLicense: Boolean = true,
-        @ProtoNumber(7) val showUpdatedTime: Boolean = false,
+        @ProtoNumber(7) val showUpdatedTime: Boolean = true,
         @ProtoNumber(8) val showCover: Boolean = true,
         @ProtoNumber(9) val showVerified: Boolean = true,
         @ProtoNumber(10) val showAntiFeatures: Boolean = true,
